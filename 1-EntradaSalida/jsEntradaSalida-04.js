0711ab54bf3c267a323487643b3ab222e21a78ed
Gaby <gabrielagrippo22@gmail.com>
2021-01-31 15:05:40 -0300

    ejercicios hasta el 6

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -4,6 +4,15 @@ y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+	//reservo espacio en memoria para guardar datos.
+	let nombre;
+	
+	//guardo en la variable nombre el texto que escribio el usuario en la ventana prompt
+	nombre=prompt("Ingrese su nombre:");
+	
+	//copio el nombre que tengo guardado en la variable nombre dentro de la caja de texto en la pagina
+	document.getElementById("txtIdNombre").value =nombre;
+
+	
 }
 
