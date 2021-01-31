b313b9dfa934ebf460361de6f90d4efa7d8a35e0
Gaby <gabrielagrippo22@gmail.com>
2021-01-31 17:24:38 -0300

    del 7 al 10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -5,5 +5,14 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+	let sueldo;
+	let aumento;
+	let resultado;
+
+	sueldo=parseFloat(document.getElementById("txtIdImporte").value);
+	
+	aumento=sueldo * 25/100 ;
+	resultado = sueldo - aumento;
+
+	document.getElementById("txtIdResultado").value = resultado;
 }
